029ae36bed71dd43a3343c51dc2ed5b76216714e
Nathan-422 <natiouswright@gmail.com>
2022-11-30 14:31:52 -0600

    continued building submit event handling

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+// import {formSubmission} from "./scriptHelper"
 // Write your JavaScript code here!
-import {formSubmission} from "./scriptHelper"
+
+console.log("Scripts working");
 
 window.addEventListener("load", function () {
+  console.log("Page loaded");
   let listedPlanets;
   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+
+  
   let listedPlanetsResponse;
   listedPlanetsResponse
     .then(function (result) {
@@ -15,15 +20,29 @@ window.addEventListener("load", function () {
       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
     });
 
-  submitButton = document.addEventListener("click", (event) => {
-    const pilotName = document.querySelector("input[name='pilotName']").value;
-    const copilotName = document.querySelector("input[name='copilotName']").value;
-    const fuelLevel = documet.querySelector("input[name='fuelLevel']").value;
-    const cargoMass = document.querySelector("input[name='cargoMass']").value;
 
-    
-
+  const form = document.querySelector("form");
+  form.addEventListener("submit", function (event) {
     event.preventDefault();
 
+    const pilotName = document.querySelector("input[name='pilotName']").value;
+    const copilotName = document.querySelector("input[name='copilotName']").value;
+    const fuelLevel = document.querySelector("input[name='fuelLevel']").value;
+    const cargoMass = document.querySelector("input[name='cargoMass']").value;
+
+    console.log(
+      `
+        Pilot: ${pilotName}
+        Co-pilot: ${copilotName}
+        Fuel: ${fuelLevel}
+        Mass: ${cargoMass}
+        `
+    );
+
+    // TODO: verify inputs are correct values
+
+    // TODO: set html elements with status
+
+    // TODO:
   });
 });
