d75166f35ddda1c1e7e99b5358d6f47625801f19
Nathan-422 <natiouswright@gmail.com>
2022-12-02 14:19:31 -0600

    formSubmission method working and can update Launch info window

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // import {formSubmission} from "./scriptHelper"
-// Write your JavaScript code here!
 
 console.log("Scripts working");
 
@@ -8,7 +7,7 @@ window.addEventListener("load", function () {
   let listedPlanets;
   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
 
-  
+  /*
   let listedPlanetsResponse;
   listedPlanetsResponse
     .then(function (result) {
@@ -19,29 +18,33 @@ window.addEventListener("load", function () {
       console.log(listedPlanets);
       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
     });
+*/
 
+  const form = document.querySelector("form[data-testid='testForm']");
 
-  const form = document.querySelector("form");
   form.addEventListener("submit", function (event) {
     event.preventDefault();
 
+    const faultyItems = document.getElementById("faultyItems");
     const pilotName = document.querySelector("input[name='pilotName']").value;
     const copilotName = document.querySelector("input[name='copilotName']").value;
     const fuelLevel = document.querySelector("input[name='fuelLevel']").value;
     const cargoMass = document.querySelector("input[name='cargoMass']").value;
+    
+    // check for all forms to have a value
+    formSubmission(document, faultyItems, pilotName, copilotName, fuelLevel, cargoMass);
+    
+    // TODO: verify inputs are correct values
+    
+    // TODO: set html elements with status
 
     console.log(
-      `
-        Pilot: ${pilotName}
-        Co-pilot: ${copilotName}
-        Fuel: ${fuelLevel}
-        Mass: ${cargoMass}
-        `
+`Pilot: ${pilotName}
+Co-pilot: ${copilotName}
+Fuel: ${fuelLevel}
+Mass: ${cargoMass}`
     );
 
-    // TODO: verify inputs are correct values
-
-    // TODO: set html elements with status
 
     // TODO:
   });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,9 +1,9 @@
 // Write your helper functions here!
-require('isomorphic-fetch');
+require("isomorphic-fetch");
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
+  // Here is the HTML formatting for our mission target div.
+  /*
                 <h2>Mission Destination</h2>
                 <ol>
                     <li>Name: </li>
@@ -17,33 +17,69 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   if (testInput === "") {
-    return "Empty"
-   } else if (isNaN(testInput)) {
-    return "Not a Number"
-   } else {
-    return "Is a Number"
-   }
+  if (testInput === "") {
+    return "Empty";
+  } else if (isNaN(testInput)) {
+    return "Not a Number";
+  } else {
+    return "Is a Number";
+  }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+  // console.log(list.lastElementChild)
+
+  const pilotCheck = validateInput(pilot) 
+  const copilotCheck = validateInput(copilot) 
+  const fuelCheck =  validateInput(fuelLevel) 
+  const cargoCheck = validateInput(cargoLevel)
+
+    if (
+        pilotCheck === "Empty" ||
+        copilotCheck === "Empty" ||
+        fuelCheck === "Empty" ||
+        cargoCheck === "Empty"
+        ) {
+            alert("All fields are required!")
+        }
+        else if (
+            pilotCheck === "Is a Number" ||
+            copilotCheck === "Is a Number" ||
+            fuelCheck === "Not a Number" ||
+            cargoCheck === "Not a Number"
+        ) {
+            alert("Make sure to enter valid information for each field!")
+        }
+        else {
+            const olPilotElement = document.getElementById("pilotStatus")
+            const olCopilotElement = document.getElementById("copilotStatus")
+            const olFuelElement = document.getElementById("fuelStatus")
+            const olCargoElement = document.getElementById("cargoStatus")
+
+            olPilotElement.innerHTML = "Hello"
+            olCopilotElement.innerHTML = "there."
+            olFuelElement.innerHTML = "General"
+            olCargoElement.innerHTML = "Konobi."
+
+            list.style.visibility = "visible"
+
+        }
+
+  
 }
 
 async function myFetch() {
-    let planetsReturned;
+  let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+  planetsReturned = await fetch().then(function (response) {});
 
-    return planetsReturned;
+  return planetsReturned;
 }
 
-function pickPlanet(planets) {
-}
+function pickPlanet(planets) {}
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
