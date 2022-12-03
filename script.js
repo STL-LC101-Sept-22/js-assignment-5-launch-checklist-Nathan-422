97da815697c0f8f2668d74a4fbd92a42643dc946
Nathan-422 <natiouswright@gmail.com>
2022-12-02 19:39:37 -0600

    I think actually fixed for the autograder

--- script.js
+++ script.js
@@ -5,26 +5,33 @@
  * Instructor: Carrie Jones
  */
 
-window.addEventListener("load", function () {
+ window.addEventListener("load", function () {
 
   let listedPlanets;
+  // Set listedPlanetsResponse equal to the value returned by calling myFetch()
   let listedPlanetsResponse = myFetch();
 
-  console.log(listedPlanetsResponse)
-
   listedPlanetsResponse.then(function (result) {
-    listedPlanets = result;
-    const planet = pickPlanet(listedPlanets);
-    addDestinationInfo(
-      document,
-      planet.name,
-      planet.diameter,
-      planet.star,
-      planet.distance,
-      planet.moons,
-      planet.image
-    );
-  });
+      listedPlanets = result;
+      // console.log(listedPlanets);
+  }).then(function () {
+      // console.log(listedPlanets);
+      // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+      const planet = pickPlanet(listedPlanets);
+      addDestinationInfo(
+        document,
+        planet.name,
+        planet.diameter,
+        planet.star,
+        planet.distance,
+        planet.moons,
+        planet.image
+        );
+  })
+
+  // listedPlanetsResponse.then(function (result) {
+  //   console.log(result.json())
+  // });
 
   // problem solving code
   document.getElementById("faultyItems").style.visibility = "hidden";
